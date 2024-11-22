4741548c3da92e2b35be6821f26888a55594e364
professor-q <username.ki.jinis@gmail.com>
2024-11-22 14:46:07 +0000

    Python - Dynamically Typed Language

---
+++ 003_datatypes.py
@@ -0,0 +1,18 @@
+name = 'professor-q'
+age = 25
+weight = 73.5
+is_coder = True
+
+print(type(age))
+print(type(weight))
+print(type(name))
+print(type(is_coder))
+
+x = 25
+print(type(x))
+x = 256.258
+print(type(x))
+x = 'Python is Dynamically typed'
+print(type(x))
+print(x)
+print('Everything in Python is Object')
