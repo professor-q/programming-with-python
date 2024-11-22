eb1eb25ed99838bfe4983ea4400bfaea33feb297
professor-q <username.ki.jinis@gmail.com>
2024-11-22 14:37:32 +0000

    Python Variables

---
+++ 002_variables.py
@@ -0,0 +1,24 @@
+age = 25
+name = 'professor-q'
+weight_in_kg = 73.5
+
+print(name)
+print(age)
+print(weight_in_kg)
+
+age = 26
+name = 'professor-m'
+weight_in_kg = 79.5
+
+print(name)
+print(age)
+print(weight_in_kg)
+
+a, b, c = 5, 25, 125
+print(a, b, c)
+
+name, age, weight_in_kg = 'professor-k', 29, 80.7
+print(name)
+print(age)
+print(weight_in_kg)
+print(name, age, weight_in_kg)
