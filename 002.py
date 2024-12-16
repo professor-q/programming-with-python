db420fba03763176410e31549db4ab4552cb0cb9
professor-q <username.ki.jinis@gmail.com>
2024-12-16 09:17:49 +0000

    Python variables

---
+++ 002.py
@@ -0,0 +1,35 @@
+age = 38
+height = 187
+number = 7
+rating = 9.2
+player_name = 'CR7'
+
+print(age)
+print(height)
+print(number)
+print(rating)
+print(player_name)
+
+print(type(age))
+print(type(height))
+print(type(number))
+print(type(rating))
+print(type(player_name))
+
+x = "John Wick"
+print(x)
+# is the same as
+x = 'John Wick'
+print(x)
+
+x = 89
+print(x)
+a = 'Python is Dynamically Typed'
+print(a)
+# The type of a variable is determined at runtime
+
+a = 4
+A = 'Mane'
+print(a)
+print(A)
+# A will not overwrite a because Python is Case-Sensitive
