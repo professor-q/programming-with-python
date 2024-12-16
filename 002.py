2cb1fb0373d9020000e2fb60e2018c1659a43af5
professor-q <username.ki.jinis@gmail.com>
2024-12-16 11:14:13 +0000

    Python Variables

--- 002.py
+++ 002.py
@@ -3,36 +3,36 @@ age = 38
 height = 187
 number = 7
 rating = 9.2
-player_name = "CR7"
+player_name = 'CR7'
 
 # Display Variable Values
-print(age)
-print(height)
-print( number)
-print(rating)
-print(player_name)
+print('Age:', age)
+print('Height:', height)
+print('Number:', number)
+print('Rating:', rating)
+print('Player Name:', player_name)
 
 # Display Data Types of Variables
-print("Type of age:", type(age))
-print("Type of height:", type(height))
-print("Type of number:", type(number))
-print("Type of rating:", type(rating))
-print("Type of player_name:", type(player_name))
+print('Type of age:', type(age))
+print('Type of height:', type(height))
+print('Type of number:', type(number))
+print('Type of rating:', type(rating))
+print('Type of player_name:', type(player_name))
 
-x = "John Wick"
-print("Double-quoted string:", x)
-# is the same as
 x = 'John Wick'
-print("Single-quoted string:", x)
+print('Single-quoted string:', x)
+# is the same as
+x = "John Wick"
+print('Double-quoted string:', x)
 
 # Dynamic Typing Example
 x = 89
 print(x)
-a = "Python is Dynamically Typed"
+a = 'Python is Dynamically Typed'
 print(a)
 
 a = 4
-A = "Mane"
+A = 'Mane'
 print(a)
 print(A)
 # A will not overwrite a because Python is Case-Sensitive
